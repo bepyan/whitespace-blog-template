25b5ec3d0cf200cad8444ca1925554eaf2d5bb3c
bepyan <bepyan@naver.com>
2024-11-17 02:18:51 +0900

    feat: add rehype & remark plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,11 @@
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypeExternalLinks from 'rehype-external-links';
+import rehypeSlug from 'rehype-slug';
+import remarkBreaks from 'remark-breaks';
+
 import { SITE } from './src/site.config';
 
 // https://astro.build/config
@@ -17,6 +22,29 @@ export default defineConfig({
           dark: 'kanagawa-dragon',
         },
       },
+      remarkPlugins: [remarkBreaks],
+      rehypePlugins: [
+        rehypeSlug,
+        [
+          rehypeAutolinkHeadings,
+          {
+            behavior: 'wrap',
+            properties: {
+              className: ['anchor'],
+            },
+          },
+        ],
+        [
+          rehypeExternalLinks,
+          {
+            properties: {
+              class: 'external link',
+            },
+            target: '_blank',
+            rel: ['noopener noreferrer'],
+          },
+        ],
+      ],
     }),
   ],
 });
