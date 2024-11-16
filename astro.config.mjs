6a67ef0de4bcddffb4d4ac36aa399bc069861377
bepyan <bepyan@naver.com>
2024-11-16 22:52:21 +0900

    feat: basic layout & rollback to tailwind v3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,12 @@
 // @ts-check
 import mdx from '@astrojs/mdx';
-import tailwindcss from '@tailwindcss/vite';
+import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
-
 import { SITE } from './src/site.config';
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE.domain,
   output: 'static',
-  vite: {
-    plugins: [tailwindcss()],
-  },
-
-  integrations: [mdx()],
+  integrations: [mdx(), tailwind()],
 });
