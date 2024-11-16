0e602f060800e79f47bfce9fd022a31b033e44c3
bepyan <bepyan@naver.com>
2024-11-16 23:20:12 +0900

    chore: clsx & tailwind-merge

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,2 +1,9 @@
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
 export const isProd = import.meta.env.PROD;
 export const isDev = import.meta.env.DEV;
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
