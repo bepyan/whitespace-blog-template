be533ffe868a5e1cf9a6c5886efc5d6d58ee0fab
bepyan <bepyan@naver.com>
2024-11-24 22:40:05 +0900

    feat: related posts

--- src/lib/mdx/post.ts
+++ src/lib/mdx/post.ts
@@ -1,4 +1,5 @@
 import { getCollection, type CollectionEntry } from 'astro:content';
+import { compareTwoStrings } from '~/lib/dice-coefficient';
 import { isProd } from '~/lib/utils';
 
 export const isDraft = (post: CollectionEntry<'posts'>) => {
@@ -16,4 +17,25 @@ export const getPostsCollection = async () => {
   return (await getCollection('posts'))
     .filter((post) => !isDraft(post))
     .sort(sortCollectionDateDesc);
+};
+
+export const getRelatedPosts = (
+  post: CollectionEntry<'posts'>,
+  postList: CollectionEntry<'posts'>[],
+) => {
+  return postList
+    .filter((p) => p.slug !== post.slug)
+    .map((p) => {
+      const tagPoint = post.data.tags
+        ? post.data.tags.filter((tag) => p.data.tags?.includes(tag)).length
+        : 0;
+      const titlePoint = compareTwoStrings(post.data.title, p.data.title);
+      return {
+        post: p,
+        similarity: tagPoint + 3.0 * titlePoint,
+      };
+    })
+    .toSorted((a, b) => b.similarity - a.similarity)
+    .map((p) => p.post)
+    .slice(0, 4);
 };
