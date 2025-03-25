b258c7f85339b8b0ccfff85a74045f0c73ef52f0
bepyan <bepyan@naver.com>
2025-03-25 21:01:33 +0900

    feat: add analytics script

--- site.config.ts
+++ site.config.ts
@@ -7,6 +7,10 @@ const config: SiteConfig = {
   description: 'minimalism coding blog build with astro',
   favicon: '/favicon.svg',
   ogImage: '/og.avif',
+  analytics: {
+    provider: 'umami',
+    websiteId: '3e44b81b-09f0-4eaf-8c30-aad1b17e7903',
+  },
 };
 
 export default config;
--- src/cfg-schema.ts
+++ src/cfg-schema.ts
@@ -1,5 +1,18 @@
 import { z } from 'astro/zod';
 
+export const analyticsSchema = z.union([
+  z.null(),
+  z.object({
+    provider: z.literal('google'),
+    tagId: z.string(),
+  }),
+  z.object({
+    provider: z.literal('umami'),
+    websiteId: z.string(),
+    host: z.string().optional(),
+  }),
+]);
+
 export const siteConfigSchema = z.object({
   domain: z.string().url(),
   title: z.string(),
@@ -8,6 +21,7 @@ export const siteConfigSchema = z.object({
   favicon: z.string(),
   ogImage: z.string().optional(),
   lang: z.string().default('en'),
+  analytics: analyticsSchema,
 });
 
 export type SiteConfig = z.input<typeof siteConfigSchema>;
